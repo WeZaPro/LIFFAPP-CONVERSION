4e6e1358b7a4b9a70c59c34359ab40d4ad9c85d4
macbook <macbook@macbooks-MacBook-Pro.local>
2024-11-15 07:47:41 +0700

    update test19

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -130,8 +130,8 @@ export default {
       // const lineLoginUrl = `https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=${clientId}&redirect_uri=${redirectUri}&state=${uri}&scope=${scope}&bot_prompt=normal&ui_locales=th-TH&disable_auto_login=true&initial_amr_display=lineqr`
 
       // ทำ redirect ไปยัง URL การล็อกอิน
-      //window.location.href = lineLoginUrl
-      window.location.href = this.lineLinkLogin
+      window.location.href = lineLoginUrl
+      // window.location.href = this.lineLinkLogin
     },
     // Initialize LIFF SDK
     // lll
