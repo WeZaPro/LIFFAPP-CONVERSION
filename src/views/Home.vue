1db8389746a31fc5bf915be8957895d94b53af8e
macbook <macbook@macbooks-MacBook-Pro.local>
2024-11-15 08:28:58 +0700

    update test25

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -135,10 +135,10 @@ export default {
 
       if (this.isDesktop === true) {
         console.log('isDesktop = true')
-        window.location.href = `https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=${clientId}&redirect_uri=${redirectUri}&state=${uri}&scope=${scope}&prompt=consent`
+        window.location.href = `https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=${clientId}&redirect_uri=${redirectUri}&state=${uri}&scope=${scope}&bot_prompt=normal&ui_locales=th-TH&disable_auto_login=true&initial_amr_display=lineqr`
       } else {
         console.log('isDesktop = false')
-        window.location.href = `https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=${clientId}&redirect_uri=${redirectUri}&state=${uri}&scope=${scope}&bot_prompt=normal&ui_locales=th-TH&disable_auto_login=true&initial_amr_display=lineqr`
+        window.location.href = `https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=${clientId}&redirect_uri=${redirectUri}&state=${uri}&scope=${scope}&prompt=consent`
       }
 
       // ทำ redirect ไปยัง URL การล็อกอิน
